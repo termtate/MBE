c3d9ecae53166c4c1ffbd276a502c89ec73f7dcc
GUIGUIzc <862121801@qq.com>
2024-09-04 17:36:56 +0800

    修复历史记录和对话功能

--- rag-system-frontend/src/main-ban.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'; // 从 'vue' 模块中导入 createApp
-import App from './components/ChatWindow.vue'; // 确保路径正确
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-const app = createApp(App);
-app.use(ElementPlus);
-app.mount('#app');
